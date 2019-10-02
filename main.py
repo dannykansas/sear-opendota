36b6c73b931231a8f9788dfa6eec41546b032c29
Danny Fowler <dan@logickc.com>
2019-10-02 00:57:18 -0700

    stash changes; basically working yaml output

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def parse_args():
     )
     parser.add_argument("output", type=FileType("w"), nargs="?", default=stdout)
     parser.add_argument(
-        "-n", "--num-teams", type=int, default=5, help="number of teams in output"
+        "-n", "--numteams", type=int, default=5, help="number of teams in output"
     )
     parser.add_argument(
         "-l",
@@ -35,18 +35,19 @@ def parse_args():
 # constants
 cumulative_xp = {}
 uri = "https://api.opendota.com/api"
-global_time = time.time()
+static_time = time.time()
 
 
 def main():
     args = parse_args()
+
     #    with open(args.output) as file:
     # TODO: put yaml together
     #    logging.basicConfig(level=logging.args("-l"))
-    get_proplayers(args)
+    get_proplayers()
 
 
-def get_proplayers(args):
+def get_proplayers():
     """
     Retrieve players from OpenDota
     """
@@ -55,26 +56,35 @@ def get_proplayers(args):
         player_xp = check_experience(player)
         cumulative_xp = add_to_scoreboard(player, player_xp)
 
-    sanitize_teams(cumulative_xp, req)
+    score_teams(cumulative_xp, req)
 
 
 def check_experience(player):
     """
-    Calculate player experience in a standardized format (diff of Unix timestamp)
+    Calculate player experience in a standardized format (diff of Unix timestamp
+    from the global_time.)
     """
     try:
         _player_history_time = player["full_history_time"]
         _corrected_time = datetime.datetime.strptime(
             _player_history_time, "%Y-%m-%dT%H:%M:%S.%fZ"
         )
-        player_xp = global_time - _corrected_time.timestamp()
+        player_xp = static_time - _corrected_time.timestamp()
     except:
+        logging.debug(
+            "Failed to calculate score for {}, setting to zero".format(
+                player["player_name"]
+            )
+        )
         player_xp = 0
 
     return player_xp
 
 
 def add_to_scoreboard(player, player_xp):
+    """
+    Summate the collective player's score into a dictionary with keys = team_id.
+    """
     if player["team_id"] not in cumulative_xp.keys():
         cumulative_xp.update({player["team_id"]: 0})
     if player["team_id"] in cumulative_xp.keys():
@@ -85,8 +95,22 @@ def add_to_scoreboard(player, player_xp):
     return cumulative_xp
 
 
-def sanitize_teams(cumulative_xp, req):
-    _arg_n = 3
+def score_teams(cumulative_xp, req):
+    """
+    Sort the top n teams, write data to YAML file.
+
+    Tradeoff Notes:
+    The YAML library here doesn't support Unicode, unlike the latest PyYAML, but
+    it does retain sort order for the yaml.dump() function, which is - for IMHO
+    kind of esoteric reasons - not fixed in the current PyYAML.
+
+    I'm also not sure what I'm missing with the all the dashes in the output here.
+    Possibly I need to use simpler data structures/a better library (or just
+    write line by line with proper indentation). Maybe just because I don't write
+    out YAML much...
+    """
+
+    _args_n = args.numteams
 
     # remove entry for team "0"
     if 0 in cumulative_xp:
@@ -95,6 +119,7 @@ def sanitize_teams(cumulative_xp, req):
     top_teams = nlargest(_arg_n, cumulative_xp, key=cumulative_xp.get)
 
     _team_list = []
+    _player_list = []
     for _team_id in top_teams:
         # DEBUG: print(_team_id, ":", cumulative_xp.get(_team_id))
         try:
@@ -106,37 +131,31 @@ def sanitize_teams(cumulative_xp, req):
                     "team_losses": team_name["losses"],
                 }
             )
-            print(team_name["name"] + " processed...")
+            logging.DEBUG("Added {} to team list.".format(team_name["name"]))
             for player in req:
                 if player["team_id"] == _team_id:
-                    _player_list = [
-                        {
-                            "persona": player["personaname"],
-                            "experience": int(check_experience(player)),
-                            "country": player["country_code"],
-                        }
-                    ]
+                    _player_list.append(
+                        [
+                            {
+                                "persona": player["personaname"],
+                                "experience": int(check_experience(player)),
+                                "country": player["country_code"],
+                            }
+                        ]
+                    )
                     _team_list.append(_player_list)
         except:
             logging.error(
-                "Failed to find Team {}, output will not include this value.".format(
-                    team_name["name"]
-                )
+                "Team {} could not be found, skipping.".format(team_name["name"])
             )
-            logging.info("")
+            logging.info("Could not find Team {} ".format(team_name["name"]))
             pass
     try:
         with open("ranking.yml", "w+") as outfile:
-            yaml.dump(_team_list, outfile, default_flow_style=False)
+            yaml.safe_dump(_team_list, outfile, default_flow_style=False)
             logging.debug("Wrote file successfully to {}".format(outfile))
     except:
         logging.error("Failed writing output to yaml file.")
-
-    build_objects(top_teams)
-
-
-def build_objects(top_teams):
-    print("done.")
 
 
 if __name__ == "__main__":
